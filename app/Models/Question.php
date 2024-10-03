1c54ab77c8bb19eab7cdc298035caab05edf55df
Khanh Luong <khanh.luong@employmenthero.com>
2024-10-03 18:47:16 +0700

    Initial commit

---
+++ app/Models/Question.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $text
+ * @property array $cue_words
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\QuestionFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Question newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Question newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Question query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Question whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Question whereCueWords($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Question whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Question whereText($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Question whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['text', 'cue_words'];
+    protected $casts = [
+        'cue_words' => 'array'
+    ];
+}
