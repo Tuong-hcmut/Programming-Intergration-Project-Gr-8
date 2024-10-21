5370e5390ed053fc7652a34cb41eec3ec228ddd7
Khanh Luong <khanh.luong@employmenthero.com>
2024-10-21 16:50:07 +0700

    Fix schedule queue

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,4 @@ use App\Jobs\GenerateQuestion;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::job(new GenerateQuestion)->hourly();
-Schedule::command('queue:work --stop-when-empty')
-    ->everyMinute()
-    ->withoutOverlapping();
+Schedule::command('queue:work --stop-when-empty')->everyMinute();
