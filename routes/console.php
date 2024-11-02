d405ac803e4e19cd99a5938ba26e12f8c2bfef1e
Khanh Luong <khanh.luong@employmenthero.com>
2024-11-02 15:58:53 +0700

    Allow queue to run for 2 mins

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use App\Jobs\GenerateQuestion;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::job(new GenerateQuestion)->everyMinute();
-Schedule::command('queue:work --stop-when-empty')->everyMinute();
+Schedule::command('queue:work --max-time=120')->everyMinute();
